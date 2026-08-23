f5edca0d7f958cf27716afb5798256420591462a
Tevi22 <tevindavis@icloud.com>
2026-08-23 19:02:14 -0400

    Fix lazy loading for favorite locations

--- src/main/java/com/indiana/foodlocator/repository/FavoriteRepository.java
+++ src/main/java/com/indiana/foodlocator/repository/FavoriteRepository.java
@@ -3,23 +3,24 @@ package com.indiana.foodlocator.repository;
 import com.indiana.foodlocator.entity.Favorite;
 import com.indiana.foodlocator.entity.FoodLocation;
 import com.indiana.foodlocator.entity.User;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FavoriteRepository
-        extends JpaRepository<Favorite, Long> {
+                extends JpaRepository<Favorite, Long> {
 
-    List<Favorite> findByUser(User user);
+        @EntityGraph(attributePaths = { "foodLocation" })
+        List<Favorite> findByUser(User user);
 
-    Optional<Favorite> findByUserAndFoodLocation(
-            User user,
-            FoodLocation foodLocation
-    );
+        Optional<Favorite> findByUserAndFoodLocation(
+                        User user,
+                        FoodLocation foodLocation);
 
-    boolean existsByUserAndFoodLocation(
-            User user,
-            FoodLocation foodLocation
-    );
+        boolean existsByUserAndFoodLocation(
+                        User user,
+                        FoodLocation foodLocation);
 }
