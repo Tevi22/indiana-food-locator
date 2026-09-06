b4167f528be47dcf37581dfadbe9db757159c4f2
Tevi22 <tevindavis@icloud.com>
2026-09-05 21:06:56 -0400

    Added successful login confirmation

--- src/main/java/com/indiana/foodlocator/config/SecurityConfig.java
+++ src/main/java/com/indiana/foodlocator/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
+                        .defaultSuccessUrl("/?loginSuccess", true)
                         .failureUrl("/login?error")
                         .permitAll())
 
